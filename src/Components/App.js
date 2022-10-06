92e5f64d3f4d2d0b38917cb34db8df9075043a73
Christopher <chrismbogo97@gmail.com>
2022-10-07 01:18:03 +0300

    Adding logo

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
 import MainContainer from "./MainContainer";
+import logo from "./logo.svg"
 
 
 function App() {
       return (
       <div className="App">
+       <img src={logo} className="App-logo" alt="logo"></img>
         <MainContainer></MainContainer>
       </div>
   );
